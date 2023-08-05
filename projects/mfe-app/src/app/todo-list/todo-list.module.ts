1e80c918c0023c162c8aa2ad6e24e3a9fdfd1454
wallace <wallace_wolv@hotmail.com>
2023-08-05 13:19:35 -0300

    feat: Adding BrowserModule and RouterModule to include route from TodoListComponent

--- projects/mfe-app/src/app/todo-list/todo-list.module.ts
+++ projects/mfe-app/src/app/todo-list/todo-list.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TodoListComponent } from './todo-list.component';
-
-
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    TodoListComponent
-  ],
+  declarations: [TodoListComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    BrowserModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: TodoListComponent,
+      },
+    ]),
+  ],
 })
-export class TodoListModule { }
+export class TodoListModule {}
